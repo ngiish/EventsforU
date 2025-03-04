5b287548f1f915f83c20e4ee69cf0b0909ba40b0
Isaac <govegeku00@gmail.com>
2025-03-04 13:42:22 +0300

    Initialization of the event planning application

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+function App() {
+
+  return (
+    <div className="app">
+      <header>
+      <h1>Event Planner</h1>
+      </header>
+      <main>
+        <p>Welcome to your first choice event planning app!</p>
+      </main>
+    </div>
+  );
+}
+
+export default App
